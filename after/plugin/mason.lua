f929abed2d6962195f2a046a40db9bb152b3acb5
Connor Willoughby <cwilloughby311@gmail.com>
2024-11-29 18:44:32 +0000

    fix: remove old code

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,9 +1,11 @@
-require("mason").setup({ ui = { icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
+	}
 })
 
 require("mason-lspconfig").setup()
@@ -12,9 +14,20 @@ require("mason-lspconfig").setup_handlers {
 	-- The first entry (without a key) will be the default handler
 	-- and will be called for each installed server that doesn't have
 	-- a dedicated handler.
-	function (server_name) -- default handler (optional)
+	function(server_name) -- default handler (optional)
 		require("lspconfig")[server_name].setup {}
 	end,
 	-- Next, you can provide a dedicated handler for specific servers.
 	-- For example, a handler override for the `rust_analyzer`:
-}
+	["lua_ls"] = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" }
+					}
+				}
+			}
+		}
+	end }
