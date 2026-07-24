ff4ab96ade4c13b014ed8914eaf336d3e832e2b6
Connor W <cwilloughby311@gmail.com>
2026-07-24 15:19:50 +0100

    feat: add more QoL settings

---
+++ lua/config/settings.lua
@@ -0,0 +1,8 @@
+-- general settings for vim
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- cant remember what this is for? 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.scrolloff = 999
